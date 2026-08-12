c5a2f90d3e1e85c43cb41c3ea785d1ba269720d2
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2026-08-12 20:57:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.optimizers import RMSprop
 
-filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://')
+filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 text = open(filepath, 'rb').read().decode(encoding='utf-8').lower()
 
